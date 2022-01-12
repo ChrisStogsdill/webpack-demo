691e79bfbe53f316c414a4af48c4938abd618f29
ChrisStogsdill <chris1stogsdill@gmail.com>
2022-01-11 20:10:43 -0600

    Finish output management tutorial

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
+import printMeTest from './print.js';
 
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
   
  // Lodash, now imported by this script
      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+     
+     btn.innerHTML = 'Click me and check the console!'
+     btn.onclick = printMeTest;
+
+     element.appendChild(btn)
     
     return element;
   }
